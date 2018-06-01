102e2ca77499a3ea7c2447394b2446a1a3fcfea3
tdonkor <tdonkor@gmail.com>
2018-06-01 14:01:02 +0100

    Add project files.

---
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using DesktopContactsApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // on first time the database is opened
+            ReadDatabase();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+
+            //display the new window
+            newContactWindow.ShowDialog();
+
+            //called after the other window has 
+            //closed or the user added so always updated
+            ReadDatabase();
+        }
+
+        void ReadDatabase()
+        {
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+
+                //holds a list of contacts  
+                var contacts = conn.Table<Contact>().ToList(); ;
+
+            }
+        }
+    }
+}
---
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //save contact
+            Contact contact = new Contact()
+            {
+                Name = txtBoxName.Text,
+                Email= txtBoxEmail.Text,
+                Phone =txtBoxPhoneNumber.Text
+            };
+
+           
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                //if the table already exists it will not be recreated
+                connection.CreateTable<Contact>();
+
+                //insert the item
+                connection.Insert(contact);
+            }
+                
+           
+
+            Close();
+
+        }
+    }
+}
